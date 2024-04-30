2dc890ca1a9b828de1644bf83f44b646acac134e
Pavithra Dhanakotti <dhanakotipavithra26@gmail.com>
2024-04-30 13:05:55 +0530

    Updating the code

---
+++ src/test/java/org/TestAutomationSelenium/TC03.java
@@ -0,0 +1,19 @@
+package org.TestAutomationSelenium;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class TC03 {
+    @Test
+    public void testVWOLogin(){
+        // system.property to setup the path of the Drivers
+        // Chrome -> ChromeDriver
+        // Edge -> EdgeDriver
+        // Firefox -> GeckoDriver
+        // Safari -> Safaridriver
+        Assert.assertTrue(false); // Main - TestNG
+
+
+    }
+
+
+
+}
